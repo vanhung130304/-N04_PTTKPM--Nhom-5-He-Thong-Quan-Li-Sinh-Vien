9b687a153155df02af9389c85e499b1f18d00fe2
huan le <huanlee2004@gmail.com>
2025-03-06 14:07:25 +0700

    test

--- javascript/main.js
+++ javascript/main.js
@@ -185,7 +185,6 @@ function temppage(){
     }
     document.getElementById("temp-page").scrollTop=0;
     document.getElementById("temp-page").style.display="block";
-<<<<<<< HEAD
 };
 function dkhoc(){
     var nd=document.getElementsByClassName("nd");
@@ -244,6 +243,3 @@ function kqdk(){
     document.getElementById("kqdk").scrollTop=0;
     document.getElementById("kqdk").style.display="block";
 };
-=======
-};
->>>>>>> 46225018b1cab323c92213d1486684fd82da8105
